866519ffbdac9e31ebd0ebcdc06da795d2ad9d75
SARFRAZ RASHEED <56587381+Sarfraz123-wq@users.noreply.github.com>
2020-10-24 18:32:22 +0500

    Add files via upload

---
+++ our website/app.js
@@ -0,0 +1,8 @@
+// let image = document.querySelectorAll('.image');
+// let teamIcon = document.querySelectorAll('.teamIcon');
+// let name = document.querySelectorAll('.name');
+// image.addEventListener('mouseover',hoja)
+// function hoja(){
+//     teamIcon.style.display = 'block';
+// }
+// Not working
